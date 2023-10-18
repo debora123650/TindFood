ffb15316f319797d47a4a0330454910d53d909ff
Debora <debora.mincheva6@gmail.com>
2023-10-18 15:49:12 +0200

    M1

---
+++ src/main/java/htwberlin/Webtechnologien/Hello.java
@@ -0,0 +1,12 @@
+package htwberlin.Webtechnologien;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class Hello {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
+
