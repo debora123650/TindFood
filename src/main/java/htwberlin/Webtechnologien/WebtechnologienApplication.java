70cb7814732a16fd88dd27b917fc7784a61f1145
debora123650 <121858611+debora123650@users.noreply.github.com>
2023-10-18 13:54:30 +0200

    Add files via upload

---
+++ src/main/java/htwberlin/Webtechnologien/WebtechnologienApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.Webtechnologien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechnologienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechnologienApplication.class, args);
+	}
+
+}
