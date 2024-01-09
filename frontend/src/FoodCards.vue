a9716ca37548847d15c504cb0d24a86784c488e9
Debora <debora.mincheva6@gmail.com>
2024-01-09 13:12:12 +0100

    Startseite beginnen

---
+++ frontend/src/FoodCards.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-for="food in foods" :key="food.id" class="food-card">
+            <img :src="food.image" alt="Food Image" />
+            <button @click="likeFood(food)">Like</button>
+            <button @click="dislikeFood(food)">Dislike</button>
+            <button @click="addToCart(food)">Add to Cart</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['foods'],
+    methods: {
+        // eslint-disable-next-line no-unused-vars
+        likeFood(food) {
+            this.$emit('choose', { action: 'like', food });
+        },
+        dislikeFood(food) {
+            this.$emit('choose', { action: 'dislike', food });
+        },
+        addToCart(food) {
+            this.$emit('choose', { action: 'addToCart', food });
+        },
+    },
+};
+</script>
+
+<style scoped>
+.food-card {
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    text-align: center;
+}
+</style>
